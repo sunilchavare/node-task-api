c624907d3d02cae47ae770ff36ad4ab8f301a0b1
sunil chavare <sunilchavare1665@gmail.com>
2026-02-07 21:39:05 +0530

    setup express server and root endpoint

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Node Project Started");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Node Task API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
