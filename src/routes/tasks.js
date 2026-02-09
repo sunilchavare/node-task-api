72a3a95a0cf2bf33d145ae4450861880fb9d1d66
sunil chavare <sunilchavare1665@gmail.com>
2026-02-09 21:10:52 +0530

    add POST /tasks endpoint

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -7,6 +7,15 @@ let tasks = [
 
 router.get("/",(req, res)=>{res.json(tasks);
 });
+router.post("/",(req,res)=>{
+    const newTask={
+        id:tasks.length+1,
+        title:req.body.title,
+        completed:false
+    };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+});
 
 
 
